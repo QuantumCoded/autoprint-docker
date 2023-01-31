42a7f33e798c76e4b8bf8d0e48e1d25a5023c9f7
QuantumCoded <mrbspony@gmail.com>
2023-01-30 20:57:58 -0600

    add print delay

--- run.sh
+++ run.sh
@@ -5,6 +5,8 @@ su -c "lpadmin -m everywhere -v ipp://$PRINTER -p printer"
 
 while inotifywait -e close_write -e create -e moved_to /var/to-print; do
     for DOC in /var/to-print/*; do
-        lp -d printer $DOC && rm $DOC
+        lp -d printer $DOC
+        while [[ -n `lpstat -W not-completed` ]]; do sleep 1; done
+        rm $DOC
     done
 done

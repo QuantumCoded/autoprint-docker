252116717670ef98adcc407d9e2cdb00c78adebc
Jeffrey <QuantumCoded@users.noreply.github.com>
2023-01-30 22:25:39 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ cupsd -f &
 sleep 10
 su -c "lpadmin -m everywhere -v ipp://$PRINTER -p printer"
 
-while inotifywait -e close_write -e create -e moved_to /var/to-print; do
+while inotifywait -e close_write -e moved_to /var/to-print; do
     for DOC in /var/to-print/*; do
         lp -d printer $DOC
         while [[ -n `lpstat -W not-completed` ]]; do sleep 1; done

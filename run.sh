12cad235a114ee70c9f26797c7630349b4a5668f
QuantumCoded <mrbspony@gmail.com>
2023-01-30 22:33:51 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,8 +5,10 @@ su -c "lpadmin -m everywhere -v ipp://$PRINTER -p printer"
 
 while inotifywait -e close_write -e moved_to /var/to-print; do
     for DOC in /var/to-print/*; do
-        lp -d printer $DOC
-        while [[ -n `lpstat -W not-completed` ]]; do sleep 1; done
-        rm $DOC
+        lp -d printer $DOC && rm $DOC
+        # echo Printing $DOC
+        # lpstat
+        # while [[ -n `lpstat -W not-completed` ]]; do sleep 1; done
+        # echo Done, REMOVE!
     done
 done
